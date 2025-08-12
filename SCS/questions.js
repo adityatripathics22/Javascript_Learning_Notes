158d5bd5f624dc5123299b0551aa7b18ea872af9
adityatripathics22 <adityatripathics22@gmail.com>
2025-08-12 22:01:35 +0530

    js syntax questions

---
+++ SCS/questions.js
@@ -0,0 +1,49 @@
+// let a = 45*2 -10;
+// console.log(a);
+
+// const newdate = new Date();
+// console.log(newdate.getFullYear());
+
+// var a = "aditya";
+// var b = "tripathi"
+// console.log(a + " " + b);
+
+// console.error("something went wrong");
+
+// var a = true;
+// console.log(typeof a);
+
+// var age = 22;
+// if(age > 18){
+//     console.log("yes");
+// }else{
+//     console.log("no")
+// }
+
+// console.log(100/0);
+
+// let a = 15;
+// a= 20;
+// console.log(a);
+
+// console.log(typeof null);
+
+// var a = "25";
+// console.log(typeof a);
+
+// for (var a = 1 ; a<51 ; a++){
+// console.log(a);
+// }
+
+// for (var i =1 ; i < 51 ; i++){
+//     if(i%2 === 0){}
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// do{
+//     console.log('a');
+// }
+// while(12>13)
+
---
+++ SCS/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+for (var i =1 ; i < 51 ; i++){
+//     if(i%2 === 0){}
+//     else{
+//         console.log(i);
+//     }
+// }
