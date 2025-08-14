737191a17a7c970a530013bd192fe30be8720ced
adityatripathics22 <adityatripathics22@gmail.com>
2025-08-14 19:00:36 +0530

    calci

---
+++ SCS/discount_calci.js
@@ -0,0 +1,14 @@
+function discountCalculator(discount){
+    return function(price){
+        return price - (price * (discount /100));
+    }
+
+}
+
+let ten = discountCalculator(10);
+let twenty = discountCalculator(20);
+// console.log(ten(100)); // should return 90
+
+console.log(discounter(100)); // should return 90
+
+console.log(discountCalculator(20)(200)); // should return 160
