6e4f2921813d6cb4f56d2f22d95257bd89798577
adityatripathics22 <adityatripathics22@gmail.com>
2025-08-14 19:18:18 +0530

    ...

--- SCS/counter.js
+++ SCS/counter.js
@@ -15,3 +15,10 @@ console.log(counter2()); // 1
 console.log(counter1()); // 3
 console.log(counter2()); // 2
 console.log(counter1()); // 4
+
+// isme function ki copy ban rahi hai aur aur pura stor eho rha hai variable mei 
+// toh jab hum functin ko call kr rhe hai toh wo purani value ko access kr rha hai
+// aur isliye count ki value change ho rhi hai
+
+//agar hum direct function call krte toh wo apni counte kki value 0 se start krta pr iss tarah agar hum
+///function ko call krte hai aur variable mei store krte hai toh wo value ko yaad rakh pata hai
