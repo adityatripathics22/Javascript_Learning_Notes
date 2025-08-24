176a65514754b07b62aab7d09b22e7fb230228f8
adityatripathics22 <adityatripathics22@gmail.com>
2025-08-24 22:09:46 +0530

    selection ways

--- SCS/DOM.js
+++ SCS/DOM.js
@@ -5,3 +5,21 @@
 // css badalna
 // attribute badalna
 // event listener lagana
+
+//there are 3 ways to select elements before we manipulate them
+// 1. getElementById
+// let element = document.getElementById("myId");
+// console.log(element); // <div id="myId">Hello World</div>
+
+// sometimes if console.log prints as a tag and not in opening format 
+// then use console.dir(element) to see all properties of the element
+
+//2. getElementsByClassName
+// let elements = document.getElementsByClassName("myClass");
+// console.log(elements); // HTMLCollection of elements with class "myClass"
+// HTMLCollection is similar to array but not exactly an array
+
+// 3. querySelector
+//let element = document.querySelector(".myClass"); // selects the first element with class "myClass"
+// let element = document.querySelector("#myId"); // selects the element with id "myId"
+// let elements = document.querySelectorAll(".myClass"); // selects all elements with class "myClass"
