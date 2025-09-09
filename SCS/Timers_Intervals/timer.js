beee9343abbe6252a9d4c71af4be1511b799a3cf
adityatripathics22 <adityatripathics22@gmail.com>
2025-09-09 23:13:50 +0530

    timers and inetrvals

---
+++ SCS/Timers_Intervals/timer.js
@@ -0,0 +1,20 @@
+// setTimeout(function(){
+//     console.log("harsh");
+// } , 1000)
+
+// let tm = setInterval(function(){
+//     console.log("harsh");
+// } , 1000)
+
+// clearInterval(tm);
+
+
+// let count = 10 ;
+
+// let interval = setInterval(function(){
+//     if(count >= 1){
+//         count--;
+//         console.log(count);
+//     }
+//     else clearInterval(interval);
+// },1000)
