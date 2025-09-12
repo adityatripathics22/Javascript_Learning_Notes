e84b3741e0d4e460eaa702b3aee5dbb783f19e38
adityatripathics22 <adityatripathics22@gmail.com>
2025-09-12 19:05:34 +0530

    storages and cookies

---
+++ SCS/Storage_&_Cookies/script.js
@@ -0,0 +1,16 @@
+//localstorage -> aapke browser ke andar data store krta jo ki browser band hone ke baad bhi delete nhi hoga
+//store aur update kaise kare----
+//localStorage.setItem("name ", "harsh");
+//data fetch kaise krte hai----
+//let val = localStorage.getItem("name ");
+//remove kaise krte hai----
+//localStorage.removeItemItem("name ");
+
+
+//sessionstorage -> ye aapka data temporarily store karta hai matlab ki tab band hua aur data gyab
+
+
+//cookies -> ye bhi data store karta hai aur aapka data browser ke 
+// cookies naam ki property mei save hota hai and ye cookie concept kam data ya light data ke liye hota hai
+
+//NOTE-> localstorage mei array wagera store nhi kr skte hai toh hum ky karenge ki JSON.stringigy() ka use krenge aur phir usko use krna ho ttoh JSON.parse() ka use krenge
