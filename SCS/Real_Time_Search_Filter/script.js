a5210912c40c3c058fe995a39f78d41320a1a3cd
adityatripathics22 <adityatripathics22@gmail.com>
2025-09-15 15:36:29 +0530

    40% JS done

--- SCS/Real_Time_Search_Filter/script.js
+++ SCS/Real_Time_Search_Filter/script.js
@@ -38,16 +38,80 @@ const users = [
 
 ]
 
+
+
 function showUsers(arr){
+    arr.forEach(function(user){
+        // Create main card div
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // Create image
+        const img = document.createElement("img");
+        img.classList.add("bg-img");
+        img.src = user.pic;
+
+        // Create blurred layer
+        const blurredLayer = document.createElement("div");
+        blurredLayer.style.backgroundImage=`url(${user.pic})`;
+        blurredLayer.classList.add("blurred-layer");
+
+        // Create content div
+        const content = document.createElement("div");
+        content.classList.add("content");
+
+        // Add h3
+        const heading = document.createElement("h3");
+        heading.textContent = user.name;
+
+        // Add paragraph
+        const para = document.createElement("p");
+        para.textContent = user.bio;
+
+        // Append h3 and p to content
+        content.appendChild(heading);
+        content.appendChild(para);
+
+        // Append all elements into card
+        card.appendChild(img);
+        card.appendChild(blurredLayer);
+        card.appendChild(content);
+
+        // Finally, append card to body (or any container)
+        document.querySelector(".cards") .appendChild(card);
+
+    })
 
 }
 
 showUsers(users);
+let a = document.querySelector(".cards");
+console.dir(a);
 
-showUsers(["harsh"])
+let inp = document.querySelector(".inp");
+inp.addEventListener("input" , function(){
+    let newusers = users.filter((users) => {
+        return users.name.startsWith(inp.value);
+    })
+    
+    const cards = document.querySelector(".cards");
+    // while (cards.firstChild) {
+    //     cards.removeChild(cards.firstChild);
+    // }
+    document.querySelector(".cards").innerHTML = "";
+
+
+    showUsers(newusers);
+});
+
+
+
+
+// showUsers(["harsh"])
 
 
 
 // sare users show karana 
 //filter karna har baar input karne pe
 //show krna filtered users
+
