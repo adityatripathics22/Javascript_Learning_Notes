59e4773f02fda8c57e716f850aa45a1cb986dd32
adityatripathics22 <adityatripathics22@gmail.com>
2025-09-30 18:00:19 +0530

    oops.js

---
+++ SCS/Phase 3/oops.js
@@ -0,0 +1,19 @@
+function CreatePencil(name, price ,color ,company){
+    this.name = name ;
+    this.price = price ;
+    this.color = color;
+    this.company=company;
+    this.write = function(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = text;
+        h1.style.color = color;
+        document.body.append(h1);
+    }
+}
+
+
+let pencil1 = new CreatePencil("aditya" , 20 , "red" , "apsara");
+let pencil2 = new CreatePencil("saurabh" , 21 , "black" , "natraj");
+
+pencil1.write("kiase ho");
+pencil2.write("mai bhadiya tum btao");
--- SCS/Phase 3/this.js
+++ SCS/Phase 3/this.js
@@ -27,15 +27,21 @@ let obj = {
 obj.sayName();
 
 /*
+THIS KI VALUE
+
 global - window 
 function - window
 method with es5 fnc - object 
 methos with es6 arrow fnc - window 
 es5 fnc inside es5 method = window 
 es6 arrow fnc inside es5 function - object
+event handler - element 
+class - blank obj
 
 kabhi bhi hume method ke function ko arrow function nhi banana hai warna wo apni value ko 
 loose kar dega aur wo window ban jayega
+
+arrow fnc humesa apni value parent se lete hai
 */
 
 //event handler
@@ -44,3 +50,41 @@ h1.addEventListener("click", function(){
     alert();
     console.log(this.style.color = "red");
 });
+
+//THIS CALL APPLY BIND
+//function ko call krte waqt aap set kar skte ho ki uski this ki value ky hogi
+
+let aditya = {
+    name: "harsh",
+}
+
+function abcde(){
+    console.log(this.name);
+}
+
+abcde.call(aditya);
+
+//apply- ye kehta hai ki aap sirf 2 hi parameters bhej skte ho
+let adityaa = {
+    name: "harsh",
+}
+
+function abcdef(a , b  ,c){
+    console.log(this , a , b , c);
+}
+
+abcdef.apply(adityaa ,[ "a" , 2 , 3]);
+
+//bind - ye function ko chalata nhi hai balki eak naya copy bana deta hai 
+//function ki jisme this ki valur object hoti hai
+let adityaaa = {
+    name: "harsh",
+}
+
+function abcdefg(a , b  ,c){
+    console.log(this , a , b , c);
+}
+
+let me = abcdefg.bind(adityaaa ,"b" , 2 , 3);
+
+me();
